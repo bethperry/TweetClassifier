527be7c232393871c5be046041111cd6a7139f4c
Beth Perry <bitmap@sbcglobal.net>
2021-01-25 16:18:36 -0600

    added ability to pass in a tweet to the url, added gitignore file, added filler text to readme.md

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+
+    # if request has args (?tweet=some_tweet)
+    if request.args:
+
+        # get the value of the arg
+        tweet = request.args.get('tweet')   
+        
+        return tweet
+
     return 'Hello World!'
 
 @app.route('/about')
